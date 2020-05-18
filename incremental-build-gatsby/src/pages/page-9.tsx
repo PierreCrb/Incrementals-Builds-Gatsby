d38de6e9e97bc6d950ce2839f89f62d025288ad2
Pierre <pierre.crbs@gmail.com>
2020-05-18 23:11:24 +0200

    Update one page

--- incremental-build-gatsby/src/pages/page-9.tsx
+++ incremental-build-gatsby/src/pages/page-9.tsx
@@ -9,7 +9,7 @@ const NinePage = (props: PageProps) => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2 ({props.path})</p>
+    <p> to page 2 ({props.path})</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
