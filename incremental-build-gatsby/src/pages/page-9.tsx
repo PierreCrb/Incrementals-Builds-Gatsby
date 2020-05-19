d666c966e254659e6c3ee3b64ec7fc9f126ab3b7
Pierre <pierre.crbs@gmail.com>
2020-05-19 14:35:24 +0200

    Add pages with multiple images

--- incremental-build-gatsby/src/pages/page-9.tsx
+++ incremental-build-gatsby/src/pages/page-9.tsx
@@ -1,17 +1,57 @@
 // Gatsby supports TypeScript natively!
-import React from "react"
+import React, { useState } from "react"
 import { PageProps, Link } from "gatsby"
-
+import Image from "../components/Image/Image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NinePage = (props: PageProps) => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p> to page 2 ({props.path})</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const NinePage = (props: PageProps) => {
+  const [imageList] = useState([
+    {
+      file: "carte.png",
+    },
+    {
+      file: "firstcommit.png",
+    },
+    {
+      file: "gatsby-illustration.png",
+    },
+    {
+      file: "icon-gatsby-netlify.png",
+    },
+    {
+      file: "jolicode-lausanne.jpg",
+    },
+    {
+      file: "jolicode.png",
+    },
+    {
+      file: "lettre.png",
+    },
+    {
+      file: "netlify.png",
+    },
+    {
+      file: "post-premieroctet.png",
+    },
+    {
+      file: "premieroctet.png",
+    },
+    {
+      file: "screenguru.png",
+    },
+  ])
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p> to page 2 ({props.path})</p>
+      {imageList.map((e, i) => (
+        <Image class="logo-img" filename={e.file} key={i} alt="image" />
+      ))}
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default NinePage
